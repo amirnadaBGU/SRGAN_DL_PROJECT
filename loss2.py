22c2e7f1e5e16f85c23b74657d2621b49afd8381
ndvam <ndvamr@gmail.com>
2025-06-17 19:06:53 +0300

    SECOND commit yeyy

--- loss2.py
+++ loss2.py
@@ -4,7 +4,7 @@ from timm import create_model
 
 
 class GeneratorLoss(nn.Module):
-    def __init__(self, vit_model_name='vit_base_patch16_224', feature_layer=5, pretrained=True, device='cuda'):
+    def __init__(self, vit_model_name='vit_base_patch16_224', feature_layer=11, pretrained=True, device='cuda'):
         super(GeneratorLoss, self).__init__()
         self.device = device
 
@@ -23,8 +23,8 @@ class GeneratorLoss(nn.Module):
             param.requires_grad = False
 
         # Normalization כפי שמתאים ל־ViT שאומן על ImageNet
-        self.register_buffer('mean', torch.tensor([0.5, 0.5, 0.5]).view(1, 3, 1, 1))
-        self.register_buffer('std', torch.tensor([0.5, 0.5, 0.5]).view(1, 3, 1, 1))
+        self.register_buffer('mean', torch.tensor([0.485, 0.456, 0.406]).view(1, 3, 1, 1))
+        self.register_buffer('std', torch.tensor([0.229, 0.224, 0.225]).view(1, 3, 1, 1))
 
         self.mse_loss = nn.MSELoss()
         self.tv_loss = TVLoss()
@@ -35,6 +35,7 @@ class GeneratorLoss(nn.Module):
         x = torch.nn.functional.interpolate(x, size=(224, 224), mode='bilinear', align_corners=False)
 
         # נורמליזציה
+        #TODO CHECK NORMALIZATION
         x = (x - self.mean) / self.std
 
         # הוצאת features מתוך ViT
